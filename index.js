5d2fa776bcd5a62ebfcfc39b2da9de2193a6f98c
Maxwell Born <borncmaxwell@gmail.com>
2024-10-19 17:03:16 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// Return the first two drivers from an array
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // Return the last two drivers from an array
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // Array containing the two functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Create a fare multiplier
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  
+  // Fare doubler function
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Fare tripler function
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function that returns either the first or last two drivers
+  const selectDifferentDrivers = function(drivers, whichDrivers) {
+    return whichDrivers(drivers);
+  };
